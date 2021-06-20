868b94935a5561b84a2602c00330d4aeacaaf5ed
MATHEUS SIQUEIRA <siqueirawm@hotmail.com>
2021-06-20 15:27:21 -0300

    href

--- mysite/views.py
+++ mysite/views.py
@@ -14,17 +14,7 @@ def page2(request):
     return render(request, "page2.html")
 
 def page3(request):
-    html = '''
-    <html>
-        <head><title>Página 3</title></head>
-        <body>
-            <h1>Python no Heroku - SOCPS</h1>
-            <h2>Página 3</h2>
-            <a href="index">index</a>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, "page3.html")
 
 def page4(request):
     return render(request, "page4.html")
